4025ec4dc72038e44e7104f02fb2c988a37042a6
Denny Temple <dev@dentemple.com>
2019-07-21 23:18:50 -0400

    Configure cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const path = require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
